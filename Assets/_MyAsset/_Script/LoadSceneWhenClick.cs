a0fbc3593e8c11d8a6b3cabe988c1297ac053c28
tonyMac <tonyMac@tonys-MacBook-Pro.local>
2019-12-05 15:54:58 +0800

    Upload3

---
+++ Assets/_MyAsset/_Script/LoadSceneWhenClick.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneWhenClick : MonoBehaviour {
+
+    public void LoadA(string scenename)
+    {
+        Debug.Log("sceneName to load: " + scenename);
+        SceneManager.LoadScene(scenename);
+    }
+
+    public void LoadRan()
+    {
+        int DataRan = Random.Range(0, 9);
+        if(DataRan > 5)
+        {
+            SceneManager.LoadScene("Scene03_1");
+        }
+        else
+        {
+            SceneManager.LoadScene("Scene03_2");
+        }
+        
+    }
+}
